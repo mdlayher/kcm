4bb647e48140849a1231d07157dc32e4c5d02f48
Matt Layher <mdlayher@gmail.com>
2022-02-28 14:06:16 -0500

    kcm: basic unprivileged test
    Signed-off-by: Matt Layher <mdlayher@gmail.com>

--- kcm_linux_test.go
+++ kcm_linux_test.go
@@ -11,7 +11,9 @@ import (
 	"golang.org/x/sys/unix"
 )
 
-func TestConn(t *testing.T) {
+func TestConnBasics(t *testing.T) {
+	// This test doesn't need extra dependencies or privileges, so it only
+	// covers a subset of the usual functionality.
 	c, err := kcm.Listen(kcm.Datagram, nil)
 	if err != nil {
 		// GitHub actions in February 2022 doesn't seem to support AF_KCM.
@@ -23,5 +25,28 @@ func TestConn(t *testing.T) {
 	}
 	defer c.Close()
 
-	// TODO(mdlayher): finish.
+	// Clone the KCM socket so we have a maximum of N sockets.
+	const n = 4
+	cs := make([]*kcm.Conn, 0, n)
+	cs = append(cs, c)
+
+	for i := 0; i < n-1; i++ {
+		cc, err := c.Clone()
+		if err != nil {
+			t.Fatalf("failed to clone KCM socket %d: %v", i, err)
+		}
+		defer cc.Close()
+
+		cs = append(cs, cc)
+	}
+
+	// Since getsockname(2) is not supported, we can't really correlate these
+	// sockets in any meaningful way. For now, just close everything out since
+	// we don't want to perform privileged operations like loading eBPF
+	// programs.
+	for i, c := range cs {
+		if err := c.Close(); err != nil {
+			t.Fatalf("failed to close cs[%d]: %v", i, err)
+		}
+	}
 }
