00a4034ef87fe6978d4d5e246f0a09575471034f
Matt Layher <mdlayher@gmail.com>
2022-02-28 10:01:26 -0500

    kcm: workarounds for lack of GitHub actions AF_KCM
    Signed-off-by: Matt Layher <mdlayher@gmail.com>

--- kcm_linux_test.go
+++ kcm_linux_test.go
@@ -4,14 +4,21 @@
 package kcm_test
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/mdlayher/kcm"
+	"golang.org/x/sys/unix"
 )
 
 func TestConn(t *testing.T) {
 	c, err := kcm.Listen(kcm.Datagram, nil)
 	if err != nil {
+		// GitHub actions in February 2022 doesn't seem to support AF_KCM.
+		if errors.Is(err, unix.EAFNOSUPPORT) {
+			t.Skipf("skipping, AF_KCM not supported: %v", err)
+		}
+
 		t.Fatalf("failed to listen: %v", err)
 	}
 	defer c.Close()
